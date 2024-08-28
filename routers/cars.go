53d73a2e12963dd3a8e3bf77734c8db7cdd9156b
Egor Nikitin <ega.nik5674@gmail.com>
2024-08-28 12:08:09 +0300

    Init cars.go

---
+++ routers/cars.go
@@ -0,0 +1,157 @@
+package routers
+
+import (
+	"encoding/json"
+	"errors"
+	"net/http"
+	"strconv"
+
+	"backend/models"
+	"backend/schemas"
+
+	"gorm.io/gorm"
+)
+
+func (app *App) CreateCar(w http.ResponseWriter, r *http.Request) {
+	// Контроллер для создания машины
+
+	var input models.Car
+
+	err := json.NewDecoder(r.Body).Decode(&input)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	userIDValue := r.Context().Value("userID")
+
+	userID := userIDValue.(uint)
+
+	car := models.Car{
+		Brand:        input.Brand,
+		Type:         input.Type,
+		FuelType:     input.FuelType,
+		Transmission: input.Transmission,
+		Amount:       input.Amount,
+		UserID:       userID,
+	}
+
+	app.DB.Create(&car)
+
+	carSerializer := schemas.Car{
+		Brand:        car.Brand,
+		Type:         car.Type,
+		FuelType:     car.FuelType,
+		Transmission: car.Transmission,
+		Amount:       car.Amount,
+		UserID:       car.UserID,
+	}
+
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(carSerializer)
+}
+
+func (app *App) GetAllCars(w http.ResponseWriter, r *http.Request) {
+	// Чтение всех машин
+
+	var cars []models.Car
+	var totalCars int64
+
+	page, err := strconv.Atoi(r.URL.Query().Get("page"))
+
+	if err != nil || page < 1 {
+		page = 1
+	}
+
+	pageSize, err := strconv.Atoi(r.URL.Query().Get("pageSize"))
+
+	if err != nil || pageSize < 1 {
+		pageSize = 10
+	}
+
+	if err := app.DB.Model(&models.Car{}).Count(&totalCars).Error; err != nil {
+		http.Error(w, "could't count cars", http.StatusInternalServerError)
+		return
+	}
+
+	offset := (page - 1) * pageSize
+
+	if err := app.DB.Limit(pageSize).Offset(offset).Find(&cars).Error; err != nil {
+		http.Error(w, "counld't find retrieve cars", http.StatusInternalServerError)
+		return
+	}
+
+	serializedCars := make([]schemas.Car, len(cars))
+
+	for i, car := range cars {
+		serializedCars[i] = schemas.Car{
+			Brand:        car.Brand,
+			Type:         car.Type,
+			FuelType:     car.FuelType,
+			Transmission: car.Transmission,
+			Amount:       car.Amount,
+			UserID:       car.UserID,
+		}
+	}
+
+	response := map[string]interface{}{
+		"cars":       serializedCars,
+		"page":       page,
+		"pageSize":   pageSize,
+		"totalPages": int((totalCars + int64(pageSize) - 1) / int64(pageSize)),
+	}
+
+	json.NewEncoder(w).Encode(response)
+}
+
+func (app *App) GetCarByID(w http.ResponseWriter, r *http.Request) {
+	// Получение машины по его ID
+
+	idStr := r.URL.Path[len("/cars/"):]
+
+	id, err := strconv.Atoi(idStr)
+
+	if err != nil {
+		http.Error(w, "invalid ID for car", http.StatusBadRequest)
+		return
+	}
+
+	var car models.Car
+
+	if err := app.DB.First(&car, id).Error; err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			http.Error(w, "car not found", http.StatusNotFound)
+		} else {
+			http.Error(w, "could't retrieve car", http.StatusInternalServerError)
+		}
+
+		return
+	}
+
+	serializedCar := schemas.Car{
+		Brand:        car.Brand,
+		Type:         car.Type,
+		FuelType:     car.FuelType,
+		Transmission: car.Transmission,
+		Amount:       car.Amount,
+		UserID:       car.UserID,
+	}
+
+	json.NewEncoder(w).Encode(serializedCar)
+}
+
+func (app *App) CarsCreateListView(w http.ResponseWriter, r *http.Request) {
+	// Котроллер для обработки POST и GET запросов на марштруте /cars
+
+	switch r.Method {
+	case http.MethodGet:
+		app.GetAllCars(w, r)
+
+	case http.MethodPost:
+		app.CreateCar(w, r)
+
+	default:
+		http.Error(w, "method not allowed", http.StatusMethodNotAllowed)
+	}
+}
