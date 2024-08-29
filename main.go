ed30f1080cafa6e1e243e6b6a532c6d478417b44
Egor Nikitin <ega.nik5674@gmail.com>
2024-08-29 16:10:37 +0300

    Update main.go

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/rs/cors"
+
 	"backend/auth"
 	"backend/database"
 	"backend/models"
@@ -23,6 +25,8 @@ func main() {
 	db.AutoMigrate(&models.User{})
 	db.AutoMigrate(&models.Car{})
 
+	http.HandleFunc("/", app.HomePageView)
+
 	http.HandleFunc("/register", app.RegisterUser)
 	http.HandleFunc("/login", app.LoginUser)
 	http.HandleFunc("/profile", auth.Authenticate(app.UserProfile))
@@ -30,6 +34,15 @@ func main() {
 	http.HandleFunc("/cars", auth.Authenticate(app.CarsCreateListView))
 	http.HandleFunc("/cars/{id}", auth.Authenticate(app.GetCarByID))
 
+	c := cors.New(cors.Options{
+		AllowedOrigins:   []string{"http://localhost:8081"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Content-Type", "Authorization"},
+		AllowCredentials: true,
+	})
+
+	handler := c.Handler(http.DefaultServeMux)
+
 	log.Println("Server started at :8080")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Fatal(http.ListenAndServe(":8080", handler))
 }
